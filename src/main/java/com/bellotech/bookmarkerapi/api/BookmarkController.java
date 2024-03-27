da34a0f0d30691b4dbea1e2f52eb316353746d75
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-03-27 20:17:08 +0000

    api commit

---
+++ src/main/java/com/bellotech/bookmarkerapi/api/BookmarkController.java
@@ -0,0 +1,24 @@
+package com.bellotech.bookmarkerapi.api;
+
+import com.bellotech.bookmarkerapi.domain.Bookmark;
+import com.bellotech.bookmarkerapi.domain.BookmarkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/bookmarks")
+@RequiredArgsConstructor
+public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    @GetMapping("")
+    public List<Bookmark> getBookmarks(){
+
+        return bookmarkService.getBookmarks();
+    }
+}
