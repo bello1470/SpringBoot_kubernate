8078d113ddf0721921f9aec928bd280c40016ec4
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-03-28 06:19:07 +0000

    bookmark ref commit

--- src/main/java/com/bellotech/bookmarkerapi/domain/Bookmark.java
+++ src/main/java/com/bellotech/bookmarkerapi/domain/Bookmark.java
@@ -2,17 +2,16 @@ package com.bellotech.bookmarkerapi.domain;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.time.Instant;
 
 @Entity
-@Data
+@Setter
+@Getter
 @Table(name = "bookmarks")
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class Bookmark {
 
     @Id
