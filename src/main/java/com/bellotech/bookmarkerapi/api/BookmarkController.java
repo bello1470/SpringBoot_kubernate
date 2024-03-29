478d867ea2360ccce96fe42cce77da3da37eed1a
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-03-29 21:30:20 +0000

    Controller commit

--- src/main/java/com/bellotech/bookmarkerapi/api/BookmarkController.java
+++ src/main/java/com/bellotech/bookmarkerapi/api/BookmarkController.java
@@ -1,16 +1,12 @@
 package com.bellotech.bookmarkerapi.api;
 
-import com.bellotech.bookmarkerapi.domain.Bookmark;
-import com.bellotech.bookmarkerapi.domain.BookmarkDTO;
+import com.bellotech.bookmarkerapi.domain.BookmarksDTO;
 import com.bellotech.bookmarkerapi.domain.BookmarkService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("api/bookmarks")
@@ -20,7 +16,7 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping("")
-    public BookmarkDTO getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page){
+    public BookmarksDTO getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page){
 
 
         return bookmarkService.getBookmarks(page);
