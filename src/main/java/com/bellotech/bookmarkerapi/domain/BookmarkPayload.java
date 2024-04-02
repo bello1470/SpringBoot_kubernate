3bbde87261869c9e352eaf1b978b759268864972
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-04-02 19:14:00 +0100

    Adding Payload to the Bookmark

---
+++ src/main/java/com/bellotech/bookmarkerapi/domain/BookmarkPayload.java
@@ -0,0 +1,18 @@
+package com.bellotech.bookmarkerapi.domain;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+
+public class BookmarkPayload {
+
+    @NotBlank(message = "title must not be emppty")
+   private String title;
+    @NotBlank(message = "url must not be empty")
+   private String url;
+
+}
