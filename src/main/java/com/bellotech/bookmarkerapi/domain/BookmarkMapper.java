3f48ebe312d60efa082436d1b792f4e284a3badd
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-04-02 19:01:04 +0100

    Map Bookmark to BookmarkDTO

---
+++ src/main/java/com/bellotech/bookmarkerapi/domain/BookmarkMapper.java
@@ -0,0 +1,16 @@
+package com.bellotech.bookmarkerapi.domain;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookmarkMapper {
+
+    public BookmarkDTO toBookmarkDTO(Bookmark bookmark){
+
+        return new BookmarkDTO(bookmark.getId(),
+                bookmark.getTitle(),
+                bookmark.getUrl(),
+                bookmark.getCreatedAt());
+    }
+}
