ea3645efadc61f19ca3e3b3ddefd7ab36a7da751
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-04-02 19:16:12 +0100

    Adding bookmark service for creating new Bookmark

--- src/main/java/com/bellotech/bookmarkerapi/domain/BookmarkService.java
+++ src/main/java/com/bellotech/bookmarkerapi/domain/BookmarkService.java
@@ -8,12 +8,16 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+
 @Service
 @Transactional
 @AllArgsConstructor
 public class BookmarkService {
 
     private final BookmarkRepository bookmarkRepository;
+
+    private final BookmarkMapper bookmarkMapper;
 
 
     @Transactional(readOnly = true)
@@ -26,5 +30,23 @@ public class BookmarkService {
 
         Page<BookmarkDTO> bookmarkDTOPage = bookmarkRepository.findAllPages(pageable);
         return new BookmarksDTO(bookmarkDTOPage);
+    }
+
+    @Transactional(readOnly = true)
+    public BookmarksDTO searchBookmark(String query, Integer page) {
+        int pageNo = page < 1 ? 0 : page - 1;
+
+        Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "createdAt");
+
+
+        Page<BookmarkDTO> bookmarkDTOPage = bookmarkRepository.searchByTitleContainingIgnoreCase(query, pageable);
+        return new BookmarksDTO(bookmarkDTOPage);
+    }
+
+    public BookmarkDTO createdBookmark(BookmarkPayload bookmarkPayload) {
+
+        Bookmark bookmark = new Bookmark(null, bookmarkPayload.getTitle(),bookmarkPayload.getUrl(), Instant.now());
+        Bookmark savedBookmark = bookmarkRepository.save(bookmark);
+        return bookmarkMapper.toBookmarkDTO(savedBookmark);
     }
 }
