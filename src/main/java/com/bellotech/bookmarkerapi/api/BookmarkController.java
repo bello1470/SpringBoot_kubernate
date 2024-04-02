18a4f37c379cb5d48cc1ee463c664583e3f1437e
bello1470 <126983812+bello1470@users.noreply.github.com>
2024-04-02 18:38:39 +0100

    Addiing create mapping to controller

--- src/main/java/com/bellotech/bookmarkerapi/api/BookmarkController.java
+++ src/main/java/com/bellotech/bookmarkerapi/api/BookmarkController.java
@@ -1,12 +1,13 @@
 package com.bellotech.bookmarkerapi.api;
 
+import com.bellotech.bookmarkerapi.domain.BookmarkDTO;
+import com.bellotech.bookmarkerapi.domain.BookmarkPayload;
 import com.bellotech.bookmarkerapi.domain.BookmarksDTO;
 import com.bellotech.bookmarkerapi.domain.BookmarkService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/bookmarks")
@@ -16,9 +17,22 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping("")
-    public BookmarksDTO getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page){
+    public BookmarksDTO getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page,
+                                    @RequestParam(name = "query", defaultValue = "") String query){
+
+        if (query == null || query.trim().isEmpty()){
+
+            return bookmarkService.getBookmarks(page);
+        }
 
 
-        return bookmarkService.getBookmarks(page);
+        return bookmarkService.searchBookmark(query,page);
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookmarkDTO createdBookmark(@RequestBody @Valid BookmarkPayload bookmarkPayload) {
+
+        return bookmarkService.createdBookmark(bookmarkPayload);
+
     }
 }
